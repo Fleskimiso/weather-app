5eb9458f64748290269562a8fa376705c1e6ba73
Fleskimiso <dawid82228@o2.pl>
2021-07-14 16:36:40 +0200

    production files

---
+++ prod/precache-manifest.610c31835e7c0f21411de3c3d15a288d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3049451e7472e47624e0",
+    "url": "/css/app.8f688881.css"
+  },
+  {
+    "revision": "7fd4c4dead55e05d2a89",
+    "url": "/css/chunk-vendors.06834b4a.css"
+  },
+  {
+    "revision": "3d5c334c765ae16a92bcaa7bef285201",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a9a71216ea7fba17a6cb",
+    "url": "/js/about.5ba9bcf6.js"
+  },
+  {
+    "revision": "3049451e7472e47624e0",
+    "url": "/js/app.e098bb6a.js"
+  },
+  {
+    "revision": "491569874e2e4ca1c940",
+    "url": "/js/chunk-2d0e5f07.6b8fbcf8.js"
+  },
+  {
+    "revision": "7fd4c4dead55e05d2a89",
+    "url": "/js/chunk-vendors.14cddb07.js"
+  },
+  {
+    "revision": "04f03e59d9481f48cf5770bbfe7e1f94",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
